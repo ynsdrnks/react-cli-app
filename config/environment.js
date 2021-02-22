35f03c64761337229056383e98bb05def3ac06ee
Yunus <34652786+ynsdrnks@users.noreply.github.com>
2021-02-22 12:59:24 +0300

    Update environment.js

--- config/environment.js
+++ config/environment.js
@@ -1,13 +1,13 @@
 //environment.js
 var environments = {
     staging: {
-      FIREBASE_API_KEY: '"AIzaSyCtZTb8fb091ba1KIe8whY4-jQ2QK1jvrI"',
+      FIREBASE_API_KEY: '"your api key"',
       FIREBASE_AUTH_DOMAIN: 'reactnativeimagerecognit-813ed.firebaseapp.com',
-      FIREBASE_DATABASE_URL: 'https://console.firebase.google.com/project/reactnativeimage/storage/reactnativeimage.appspot.com/files',
+      FIREBASE_DATABASE_URL: 'your database url',
       FIREBASE_PROJECT_ID: 'soy-extension-300812',
       FIREBASE_STORAGE_BUCKET: 'reactnativeimagerecognit-813ed.appspot.com',
       FIREBASE_MESSAGING_SENDER_ID: '144996793514',
-      GOOGLE_CLOUD_VISION_API_KEY: 'AIzaSyAPMNiwNAhTBW2sFitOVABzk6spfmiXfRI'
+      GOOGLE_CLOUD_VISION_API_KEY: 'add key'
     },
     production: {
       // Warning: This file still gets included in your native binary and is not a secure way to store secrets if you build for the app stores. Details: https://github.com/expo/expo/issues/83
